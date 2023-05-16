580e05f9169bacce34fdde7177ee12730f88a6b2
G808 <hucciallstars@gmail.com>
2023-05-16 21:24:43 +0300

    Fetching todos from firestore db

--- pages/index.js
+++ pages/index.js
@@ -20,8 +20,6 @@ import { MdDeleteForever } from 'react-icons/md';
 import { GoSignOut } from 'react-icons/go';
 import { useState } from 'react';
 
-const arr = [1, 2, 3];
-
 export default function Home() {
   const { authUser, isLoading, signOut } = useAuth();
   const [todos, setTodos] = useState([]);
@@ -33,7 +31,41 @@ export default function Home() {
     if (!isLoading && !authUser) {
       router.push('/login');
     }
+    if (!!authUser) {
+      fetchTodos(authUser.uid);
+    }
   }, [authUser, isLoading]);
+
+  //add todo
+  const addTodo = async () => {
+    try {
+      const docRef = await addDoc(collection(db, 'todos'), {
+        author: authUser.uid,
+        content: todoInput,
+        done: false,
+      });
+      fetchTodos(authUser.uid);
+      setTodoInput('');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  //fetch todos
+  const fetchTodos = async (uid) => {
+    try {
+      const q = query(collection(db, 'todos'), where('author', '==', uid));
+      const querySnapshot = await getDocs(q);
+      let data = [];
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, ' => ', doc.data());
+        data.push({ ...doc.data(), id: doc.id });
+      });
+      setTodos(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return !authUser ? (
     <Loader />
@@ -58,34 +90,44 @@ export default function Home() {
               type='text'
               className='font-semibold placeholder:text-gray-500 border-[2px] border-black h-[60px] grow shadow-sm rounded-md px-4 focus-visible:outline-main text-lg transition-all duration-300 text-center md:text-left'
               autoFocus
+              onChange={(e) => setTodoInput(e.target.value)}
+              value={todoInput}
             />
-            <button className='w-[60px] h-[60px] rounded-md bg-main flex justify-center items-center cursor-pointer transition-all duration-300 hover:bg-black/[0.8]'>
+            <button
+              className='w-[60px] h-[60px] rounded-md bg-main flex justify-center items-center cursor-pointer transition-all duration-300 hover:bg-black/[0.8]'
+              onClick={addTodo}
+            >
               <AiOutlinePlus size={30} color='#fff' />
             </button>
           </div>
         </div>
         <div className='my-10'>
-          {arr.map((todo, index) => (
-            <div className='flex items-center justify-between mt-4'>
-              <div className='flex items-center gap-3'>
-                <input
-                  id={`todo-${index}`}
-                  type='checkbox'
-                  className='w-4 h-4 accent-main rounded-lg'
-                />
-                <label htmlFor={`todo-${index}`} className='font-medium'>
-                  This is my first todo
-                </label>
-              </div>
+          {todos.length > 0 &&
+            todos.map((todo) => (
+              <div
+                className='flex items-center justify-between mt-4'
+                key={todo.id}
+              >
+                <div className='flex items-center gap-3'>
+                  <input
+                    id={`todo-${todo.id}`}
+                    type='checkbox'
+                    className='w-4 h-4 accent-main rounded-lg'
+                    checked={todo.done}
+                  />
+                  <label htmlFor={`todo-${todo.id}`} className='font-medium'>
+                    {todo.content}
+                  </label>
+                </div>
 
-              <div className='flex items-center gap-3'>
-                <MdDeleteForever
-                  size={24}
-                  className='text-red-400 hover:text-red-600 cursor-pointer'
-                />
+                <div className='flex items-center gap-3'>
+                  <MdDeleteForever
+                    size={24}
+                    className='text-red-400 hover:text-red-600 cursor-pointer'
+                  />
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     </main>
