c321b44a8911ba4973540d1ccf212ad97984fdb5
G808 <hucciallstars@gmail.com>
2023-05-18 00:27:23 +0300

    Favicon

--- pages/index.js
+++ pages/index.js
@@ -108,7 +108,7 @@ export default function Home() {
       <div className='max-w-3xl mx-auto mt-10 p-8'>
         <div className='bg-white -m-6 p-3 sticky top-0'>
           <div className='flex justify-center flex-col items-center'>
-            <span className='text-7xl mb-10'>📝</span>
+            <span className='text-7xl mb-10'>🗒️</span>
             <h1 className='text-5xl md:text-7xl font-bold'>Easy Todos</h1>
           </div>
           <div className='flex items-center gap-2 mt-10'>
